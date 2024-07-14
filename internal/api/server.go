2dbca61a078943533992c3e51a96b46b81881b34
emmadev <macanthonyemmanuel12@gmail.com>
2024-07-15 00:31:08 +0100

    updates

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,19 +1,33 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Emmanuel-MacAnThony/gocommerce/config"
 	"github.com/Emmanuel-MacAnThony/gocommerce/internal/api/rest"
 	"github.com/Emmanuel-MacAnThony/gocommerce/internal/api/rest/handlers"
+	"github.com/Emmanuel-MacAnThony/gocommerce/internal/domain"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func StartServer(config config.AppConfig) {
 	app := fiber.New()
 	app.Get("/health", HealthCheck)
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("database connection failed %v \n", err)
+	}
+	log.Println("database connected 🚀🚀🚀")
+
+	// run migration
+	db.AutoMigrate(&domain.User{})
+
 	rh := &rest.RestHandler{
 		App: app,
+		DB:  db,
 	}
 	setupRoutes(rh)
 	app.Listen(config.ServerPort)
